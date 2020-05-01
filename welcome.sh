3fa441006ba66c65d5a772dffe2a41f099e5993d
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-01 17:10:09 +0530

    added  part time or full time UC

--- welcome.sh
+++ welcome.sh
@@ -1,11 +1,22 @@
-
-
-#! /bin/bash 
-ispresent=1;
-	status=$(( $RANDOM%2 ));
-	if [ $status -eq $ispresent ]
+#! /bin/bash
+echo "welcome to employee wage calculation program" 
+wagePerHour=20
+dayHour=8
+isPorF=1;
+	statusofPorF=$(( $RANDOM%2 ));
+	if [ $statusofPorF -eq $isPorF ]
+	then
+ 	echo "employee is part time"
+	else 
+	echo "employee is full time"
+	fi
+ 	dailyWage=$(( $wagePerHour*$dayHour ))
+	ispresent=1
+	status=$(( $RANDOM%2 ))
+	if [ $status -eq $ispresent ] &&  [ $statusofPorF -eq $isPorF ]
 	then
  	echo "employee is present"
+	echo "employee earned Rs" $dailyWage
 	else
  	echo "employee is absent"
-	fi
+        fi
