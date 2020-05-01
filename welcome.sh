d4647f73648833b058c3354f8410b0f43bf6e330
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-01 16:26:00 +0530

    Daily wage UC added

--- welcome.sh
+++ welcome.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-echo "welcome to employee wage calculation program"
 
-#! /bin/bash 
-ispresent=1;
-	status=$(( $RANDOM%2 ));
+wagePerHour=20
+dayHour=8
+dailyWage=$(( $wagePerHour*$dayHour ))
+	ispresent=1
+	status=$(( $RANDOM%2 ))
 	if [ $status -eq $ispresent ]
 	then
  	echo "employee is present"
+	echo "employee earned Rs" $dailyWage
 	else
  	echo "employee is absent"
+        fi
